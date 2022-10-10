1e83d23c8573771858b3143ef710a37022c73344
2Nadim <nadimmahamud661@gmail.com>
2022-10-10 19:01:00 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Main from './components/Layouts/Main';
+import Statistics from './components/Statistics/Statistics';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+
+      ]
+    }
+  ])
+  return (
+    <div className="App">
+      <div>
+
+        <RouterProvider router={router}></RouterProvider>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav>
+            <div>
+                <h2>Quiz Crackers</h2>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
